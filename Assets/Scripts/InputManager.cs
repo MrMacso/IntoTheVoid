926a10be232ce578aeae9e1ddeba067834bb4d32
Csaba Maczo <maczocsaba@gmail.com>
2024-04-09 16:15:33 +0100

    adding character movement

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float _acceleration = 5f;
+    [SerializeField] float _deceleration = 5f;
+    [SerializeField] float _maxSpeed = 20f;
+    Rigidbody _rb;
+    PlayerInput _playerInput;
+    PlayerControls _playerControls;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _playerInput = GetComponent<PlayerInput>();
+
+        _playerControls = new PlayerControls();
+        _playerControls.Player.Enable();
+        _playerControls.Player.Cast.performed += Cast_performed;
+    }
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = _playerControls.Player.Movement.ReadValue<Vector2>();
+        if(inputVector != Vector2.zero )
+        {
+            _rb.AddForce(new Vector3(inputVector.x, 0f, inputVector.y) * _acceleration, ForceMode.Force);
+
+            if(_rb.velocity.magnitude > _maxSpeed)
+            {
+                _rb.velocity = _rb.velocity.normalized * _maxSpeed;
+            }
+            if(Vector3.Dot((Vector3)inputVector, _rb.velocity) < 0f )
+            {
+                _rb.AddForce(_rb.velocity * -_deceleration, ForceMode.Force);
+            }
+        }
+        else
+        {
+            _rb.AddForce(_rb.velocity * - _deceleration, ForceMode.Force);
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Cast_performed(InputAction.CallbackContext context)
     {
-        
+        if (context.performed)
+        {
+            Debug.Log("Cast " + context.phase);
+        }
     }
 }
